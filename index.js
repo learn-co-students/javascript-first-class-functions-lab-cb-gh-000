715244c120f7e9b2591b20067099c17d554590c5
Robert Wayne <robertjwayne@gmail.com>
2017-06-03 19:16:12 -0400

    Create function multiplier(firstValue, secondValue)

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ function createMultiplier(multiplierValue) {
   return (number) => number * multiplierValue
 }
 
+function multiplier(firstValue, secondValue) {
+  return firstValue * secondValue
+}
+
 var doubler = createMultiplier(2)
 var tripler = createMultiplier(3)
