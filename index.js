eb4652430299455d80bc3ce1b1aa904014b62bbb
Robert Wayne <robertjwayne@gmail.com>
2017-06-03 19:14:38 -0400

    Create function createMultiplier(multiplierValue), doubler and tripler

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function countdown(callback) {
   window.setTimeout(() => callback(), 2000)
 }
+
+function createMultiplier(multiplierValue) {
+  return (number) => number * multiplierValue
+}
+
+var doubler = createMultiplier(2)
+var tripler = createMultiplier(3)
