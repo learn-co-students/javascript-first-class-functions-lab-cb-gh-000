5efead2d21fbefcb20471d8263f655820c99574e
Robert Wayne <robertjwayne@gmail.com>
2017-06-03 19:17:59 -0400

    Create doublerWithBind and triplerWithBind

--- index.js
+++ index.js
@@ -12,3 +12,6 @@ function multiplier(firstValue, secondValue) {
 
 var doubler = createMultiplier(2)
 var tripler = createMultiplier(3)
+
+var doublerWithBind = multiplier.bind(null, 2)
+var triplerWithBind = multiplier.bind(null, 3)
