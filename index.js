db57acce537f9df9584b2b32c3a1f35a6d8cea80
Trisha Milan <101035074@student.swin.edu.au>
2017-07-12 15:48:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function countdown(callback) {
+  window.setTimeout(callback, 2000);
+}
+
+function multiplier(arg1, arg2) {
+  return arg1 * arg2;
+}
+
+var doubler = createMultiplier(2);
+var tripler = createMultiplier(3);
+
+function createMultiplier(multiplierValue) {
+  return function(value) {
+    return value * multiplierValue;
+  }
+}
+
+var doublerWithBind = multiplier.bind(2, null);
+var triplerWithBind = multiplier.bind(3, null);
