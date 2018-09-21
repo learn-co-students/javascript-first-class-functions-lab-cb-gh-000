f698afb80c5c248e39ac109019ba7d50c7e0cacd
Yaro Schiffelers <yaroschiffelers@gmail.com>
2018-09-21 08:24:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+// 1
+const countdown = (cb) => {
+  window.setTimeout(cb, 2000)
+}
+
+// 2
+const createMultiplier = (multi) => {
+    return function multiplyNumber(number) {
+      return multi * val
+    }
+}
+
+const doubler = createMultiplier(2)
+const tripler = createMultiplier(3)
+
+// 3
+const multiplier = (multiplierValue, value) => {
+  return function multiplyNumber(multiplierValue, value) {
+    return multiplierValue * value
+  }
+}
+
+const doublerWithBind = multiplier.bind(null, 2)
+const triplerWithBind = multiplier.bind(null, 3)
