da0d51e7cca54a72330bd01095f1d924dfd43d9d
IVAN V <ivanvalette@hotmail.com>
2019-04-26 16:15:30 +0000

    Done.

--- index.js
+++ index.js
@@ -5,8 +5,19 @@ function countdown(callback)
 
 function createMultiplier(mult)
 {
-  return ((mult, n) => {return mult * n})
+  return ((n) => {return mult * n})
 }
 
 var double = createMultiplier(2)
-console.log(double())
+var triple = createMultiplier(3)
+console.log(double(5))
+console.log(triple(10))
+
+function multiplier(a, b) {
+  return a * b;
+}
+
+var doublerWithBind = multiplier.bind(null, 2, 3, 4);
+var triplerWithBind = multiplier.bind(null, 3);
+
+console.log(doublerWithBind())
