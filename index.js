1805521a96fc14edd0d863791dabb4c0d49cf729
Armintas Zadeika <az778@york.ac.uk>
2020-05-18 18:38:27 +0000

    Done.

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ function countdown(callback){
   window.setTimeout(callback,2000);
 }
 function createMultiplier(multiplierValue){
-
+  return function(value){
+    return multiplierValue*value;
+  }
 }
 function multiplier(multiplierValue,value){
   return multiplierValue*value;
 }
-function doublerWithBind(){
-  return multiplier.bind(2);
-}
+var doubler = createMultiplier(2)
+var tripler = createMultiplier(3)
+var doublerWithBind = multiplier.bind(null,2)
+var triplerWithBind = multiplier.bind(null,3)
